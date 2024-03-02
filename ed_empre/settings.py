ec1897c11f9341882048478a5c9fc66d7bec78a7
EstebanRP98 <davidesteb98@gmail.com>
2024-03-02 15:32:32 -0500

    conexion con el bot 7

--- ed_empre/settings.py
+++ ed_empre/settings.py
@@ -83,9 +83,8 @@ WSGI_APPLICATION = "ed_empre.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
