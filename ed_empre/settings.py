d75cc73beac740ba7d35344e55b10c3a5238bb35
EstebanRP98 <davidesteb98@gmail.com>
2024-03-02 15:27:15 -0500

    conexion con el bot 4

--- ed_empre/settings.py
+++ ed_empre/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
-import os
-from decouple import config
-import firebase_admin
 from firebase_admin import credentials
+import firebase_admin
+from decouple import config
+import os
+from pathlib import Path
+import sys
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
